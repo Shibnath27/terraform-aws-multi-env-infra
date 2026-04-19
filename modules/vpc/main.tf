43bdd465190a117f2f2cb817d4ee3d9411050fd3
Shibnath27 <shibnathdas27x@gmail.com>
2026-04-19 12:45:24 +0530

    update vpc

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -35,9 +35,3 @@ resource "aws_route_table_association" "assoc" {
   route_table_id = aws_route_table.rt.id
 }
 
-resource "aws_route" "internet_access" {
-  route_table_id         = aws_route_table.public.id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.igw.id
-}
-
