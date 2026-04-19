c0612ce38ce159b0be3fba5d3a3e8bb24a2a9d7e
Shibnath27 <shibnathdas27x@gmail.com>
2026-04-19 13:44:09 +0530

    fix az in vpc

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -6,6 +6,8 @@ resource "aws_vpc" "this" {
   }
 }
 
+data "aws_availability_zones" "available" {}
+
 resource "aws_subnet" "public" {
   vpc_id     = aws_vpc.this.id
   cidr_block = var.public_subnet_cidr
